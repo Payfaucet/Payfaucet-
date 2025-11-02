c033ff159a5ba7e0fb128e315f83a2aefecb5d0f
Payfaucet <ryzefn247@gmail.com>
2025-11-02 20:24:35 +0400

    Delete scripts/exchange directory

--- scripts/exchange/get_exchange_schedule.sh
+++
@@ -1,3 +0,0 @@
-curl --request GET \
-     --url https://trading-api.kalshi.com/trade-api/v2/exchange/schedule \
-     --header 'accept: application/json'
--- scripts/exchange/get_exchange_status.sh
+++
@@ -1,3 +0,0 @@
-curl --request GET \
-     --url https://trading-api.kalshi.com/trade-api/v2/exchange/status \
-     --header 'accept: application/json'
